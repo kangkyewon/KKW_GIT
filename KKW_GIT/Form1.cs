1ca19e826f860d50b7514a4ba66eae7bd104787f
kangkyewon <rkdrpdnjs1@gmail.com>
2026-03-10 13:13:29 +0900

    a

--- KKW_GIT/Form1.cs
+++ KKW_GIT/Form1.cs
@@ -23,15 +23,6 @@ namespace KKW_GIT
             int b;
             int c;
             int d;
-            int e;
-            int f;
-            int g;
-            int h;
-            int i;
-            int j;
-            int k;
-            int l;
-            int m;
         }
     }
 }
